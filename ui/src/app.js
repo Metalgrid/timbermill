3b223d88a5819fc2728534993276add5452313a1
Iskren Hadzhinedev <i.hadzhinedev@gmail.com>
2021-02-15 08:17:11 +0530

    Add web ui

---
+++ ui/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {GiAxeInStump} from 'react-icons/gi';
+
+export default function App() {
+    return (
+      <div>
+        Timbermill <GiAxeInStump />
+      </div>
+    );
+}
